759145bb14e4c69eb8f51e9efe964b76745b823e
Raymond Camden <raymondcamden@gmail.com>
2023-10-24 08:37:28 -0500

    new demo

--- translate/src/index.js
+++ translate/src/index.js
@@ -8,8 +8,25 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { Ai } from '@cloudflare/ai';
+
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request, env) {
+		const ai = new Ai(env.AI);
+
+		const { searchParams } = new URL(request.url);
+		let text = searchParams.get('text');
+
+		const input = { text, source_lang:'en', target_lang: 'fr' };
+
+		const response = await ai.run('@cf/meta/m2m100-1.2b', input);
+
+		return new Response(JSON.stringify(response), {
+			headers: {
+				'Content-Type':'application/json;charset=UTF-8',
+				'Access-Control-Allow-Origin':'*'
+			}
+		});
+
 	},
 };
