f94d57b52954ca4969368b5ebf276affb86384c8
Raymond Camden <raymondcamden@gmail.com>
2023-11-17 10:17:39 -0600

    misc

--- routetest/src/index.js
+++ routetest/src/index.js
@@ -1,15 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Router, json, createCors, error } from 'itty-router';
+
+const { preflight, corsify } = createCors();
+
+const router = Router();
+
+const CATS = [
+	{name:'Luna', gender:'female', age: 12 },
+	{name:'Pig', gender:'female', age: 10},
+	{name:'Elise', gender:'female', age: 111},
+	{name:'Zelda', gender:'female', age: 1},
+	{name:'Grace', gender:'female', age: 13},
+];
+
+router.all('*', preflight);
+
+router.get('/', async (req) => {
+	return CATS;
+});
+
+router.get('/:name', async (req) => {
+	const name = req.params.name;
+	return CATS.filter(c => c.name.toUpperCase() === name.toUpperCase());
+});
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		return router.handle(request).then(json).catch(error).then(corsify);
 	},
 };
